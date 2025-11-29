b858d52acf9877cf4f97899fbbed7e29e8b6c6d5
valentyn <osiptsev.valik@gmail.com>
2025-11-29 04:23:16 +0200

    fix deploy

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  // host: env("HOST", "0.0.0.0"),
-  // port: env.int("PORT", 1337),
-  url: "https://grounded-star-1867245af7.strapiapp.com/",
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("STRAPI_URL", "https://grounded-star-1867245af7.strapiapp.com"),
   app: {
     keys: env.array("APP_KEYS"),
   },
